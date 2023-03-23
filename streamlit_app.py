6380ca5c7c4b4e58ba8568051980458c9bd820c8
KHALID AIT ZI <105863366+khalidaitzi@users.noreply.github.com>
2023-03-23 10:16:50 +0000

    Update streamlit_app.py
    add sub headers

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,6 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
+st.subheader('Omega 3 & Blueberry Oatmeal')
+st.subheader('Kale, Spinach & Rocket Smoothie')
+st.subheader('Hard-Boiled Free-Range Egg')

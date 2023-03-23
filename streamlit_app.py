9836123fd83addfeb495150dcbdd317ef39052dc
KHALID AIT ZI <105863366+khalidaitzi@users.noreply.github.com>
2023-03-23 10:23:09 +0000

    Update streamlit_app.py
    change subheader to text

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.subheader('Omega 3 & Blueberry Oatmeal')
-streamlit.subheader('Kale, Spinach & Rocket Smoothie')
-streamlit.subheader('Hard-Boiled Free-Range Egg')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
